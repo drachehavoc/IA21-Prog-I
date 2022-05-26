6f8551decb9bff21fdf3c955b3ea4bca6cf56ea1
Varela <caldodecode@gmail.com>
2022-05-26 10:03:57 -0300

    ...

--- turma-01/000-introducao-poo/objeto de estudos POO - atari breakout/main.js
+++ turma-01/000-introducao-poo/objeto de estudos POO - atari breakout/main.js
@@ -2,18 +2,32 @@ const canvas = document.createElement("canvas")
 const context = canvas.getContext("2d")
 
 canvas.width = 800
-canvas.height = 600
+canvas.height = 650
 
-let y = 0
-let barSize = 100
-let directionY = 1
+let x = 100
+let barWidth = 100
+let barHeight = 20
+let directionX = 0
+
+document.addEventListener("keydown", ev => {
+    if (!ev.repeat && ev.code == "KeyD") 
+        return directionX = 2
+
+    if (!ev.repeat && ev.code == "KeyA")
+        return directionX = -2
+})
+
+document.addEventListener("keyup", ev => {
+    if (ev.code == "KeyD" || ev.code == "KeyA")
+        return directionX = 0
+})
 
 function mainLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height)
-    context.fillRect(10, y, 20, barSize)
-    y += directionY
-    if (y <= 0 || y >= canvas.height - barSize) {
-        directionY *= -1
+    context.fillRect(x, canvas.height-barHeight, barWidth, barHeight)
+    x += directionX
+    if (x <= 0 || x >= canvas.width - barWidth) {
+        directionX = 0
     }
     requestAnimationFrame(mainLoop)
 }
