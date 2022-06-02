0ba55b37f349df457d7449f77870211de52e2a49
Varela <caldodecode@gmail.com>
2022-06-02 11:22:26 -0300

    ...

--- turma-02/000-introducao-poo/objeto de estudos POO - atari pong/Bar.ts
+++ turma-02/000-introducao-poo/objeto de estudos POO - atari pong/Bar.ts
@@ -1,28 +1,49 @@
 export class Bar {
+    private _canvas: HTMLCanvasElement
+    private _context: CanvasRenderingContext2D
     private _height: number
     private _width: number
     private _speed: number
     private _direction: number = 0
     private _position: number = 100
     private _color: string
+    private _offset: number
 
     constructor(
+        canvas: HTMLCanvasElement,
         height: number,
         width: number,
         speed: number,
         color: string,
+        offset?: number 
     ) {
+        this._canvas = canvas
+        this._context = canvas.getContext("2d")
         this._height = height
         this._width = width
         this._speed = speed
         this._color = color
-     }
+        this._offset = offset ?? 0
+    }
 
-     moveUp() { }
+    movingUp() {
+        this._direction = -this._speed
+    }
+    
+    movingDown() {
+        this._direction = this._speed
+    }
 
-     moveDown() { }
+    stopMove() {
+        this._direction = 0
+    }
 
-     render() { }
+    render() {
+        this._context.fillStyle = this._color
+        this._context.fillRect(this._offset, this._position, this._width, this._height)
+        this._position += this._direction
+        if (this._position <= 0 || this._position >= this._canvas.height - this._height) {
+            this._direction = 0
+        }
+    }
 }
-
-const bar = new Bar(50, 10, 5, "red")
--- turma-02/000-introducao-poo/objeto de estudos POO - atari pong/main.js
+++ turma-02/000-introducao-poo/objeto de estudos POO - atari pong/main.js
@@ -1,41 +1,84 @@
+import { Bar } from "./Bar.ts"
+
 const canvas = document.createElement("canvas")
 const context = canvas.getContext("2d")
 
-canvas.width = 300
+canvas.width = 400
 canvas.height = 300
 
-let y = 100
-let directionY = 0
-let barHeight = 50
-let barWidth = 10
-let speed = 5
+const bar = new Bar(canvas, 50, 10, 5, "green")
+const bar2 = new Bar(canvas, 50, 10, 5, "pink", canvas.width - 10)
 
 document.addEventListener("keydown", ev => {
   if (ev.repeat)
     return
 
   if (ev.code == "KeyS")
-    return directionY = speed
+    return bar.movingDown()
 
   if (ev.code == "KeyW")
-    return directionY = -speed
+    return bar.movingUp()
+
+
+  if (ev.code == "ArrowDown")
+    return bar2.movingDown()
+
+  if (ev.code == "ArrowUp")
+    return bar2.movingUp()
 })
 
-document.addEventListener("keyup", ev => { 
+document.addEventListener("keyup", ev => {
   if (ev.code == "KeyS" || ev.code == "KeyW")
-    return directionY = 0
+    return bar.stopMove()
+
+  if (ev.code == "ArrowDown" || ev.code == "ArrowUp")
+    return bar2.stopMove()
 })
 
 function mainLoop() {
   context.clearRect(0, 0, canvas.width, canvas.height)
-  context.fillRect(10, y, barWidth, barHeight)
-  y += directionY
-  if(y <= 0 || y >= canvas.height - barHeight) {
-    directionY = 0 
-  }
-  requestAnimationFrame(mainLoop)
+  bar.render()
+  bar2.render()
+  requestAnimationFrame(mainLoop) // recursive exec of mainLoop
 }
 
-requestAnimationFrame(mainLoop)
+requestAnimationFrame(mainLoop) // first exec of mainLoop
 
 document.body.append(canvas)
+
+
+// let y = 100
+// let directionY = 0
+// let barHeight = 50
+// let barWidth = 10
+// let speed = 5
+
+// document.addEventListener("keydown", ev => {
+//   if (ev.repeat)
+//     return
+
+//   if (ev.code == "KeyS")
+//     return directionY = speed
+
+//   if (ev.code == "KeyW")
+//     return directionY = -speed
+// })
+
+// document.addEventListener("keyup", ev => {
+//   if (ev.code == "KeyS" || ev.code == "KeyW")
+//     return directionY = 0
+// })
+
+// function mainLoop() {
+//   context.clearRect(0, 0, canvas.width, canvas.height)
+//   context.fillRect(10, y, barWidth, barHeight)
+//   y += directionY
+//   if(y <= 0 || y >= canvas.height - barHeight) {
+//     directionY = 0
+//   }
+//   requestAnimationFrame(mainLoop)
+// }
+
+// requestAnimationFrame(mainLoop)
+
+// document.body.append(canvas)
